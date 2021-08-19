a7ba4f0ed4f9ead4aa1929e623a516c404113006
afahernandes <afahernandes@gmail.com>
2021-08-19 23:37:29 +0700

    Styling css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-// import components here
+import React from 'react'
+import Form from './components/Form';
 
 function App() {
   return (
-    // code inside div
     <div>
-      
+      <Form/>
     </div>
-  );
+  )
 }
 
 export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,42 @@
-// create style here
-
+const style = {
+  form: {
+    margin: "16px 20% 0",
+  }, 
+  formGroup: {
+    marginBotton: "16px",
+  },
+  formLabel: {
+    marginBotton: "8px",
+    display:"block", 
+  },
+  InputLabel: {
+    display: "block",
+    width: "100%",
+    padding: ".375rem .75rem",
+    fontSize: "1rem",
+    lineHeight: 1.5,
+    color: "blue",
+    backroundColor: "white",
+    border: "1px solid #ced4da",
+    borderRadius: "5px",
+    outline:0,  
+  },
+};
 // create Form component
 function Form() {
   return (
     // code inside div
     <div>
-      {/* form */}
+      <form style={style.form}>
+        <div style={style.formGroup}>
+          <h3 style={{ fontStyle: "bold", color: "red" }}>Login</h3>
+
+          <label htmlFor="username" style={style.formLabel}>Username</label>
+          <input id="username" style={style.InputLabel} placeholder="Input Username" />
+        </div>
+      </form>
     </div>
-  )
+  );
 }
 
-export default Form
+export default Form;
